038dc3c473d6b73983345fcc5f4367a6c86638f7
Richard Garcia <code.richardy@gmail.com>
2025-06-28 21:05:42 -0300

    this commit introduces new global usings

--- Source/Comanda.WebApi/Usings.cs
+++ Source/Comanda.WebApi/Usings.cs
@@ -5,6 +5,7 @@ global using System.Text.Json;
 /* global usings for the Microsoft namespaces here */
 
 global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Authorization;
 global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 global using Microsoft.AspNetCore.Authentication.JwtBearer;
 global using Microsoft.IdentityModel.Tokens;
@@ -12,6 +13,7 @@ global using Microsoft.IdentityModel.Tokens;
 /* global usings for the Comanda namespaces here */
 
 global using Comanda.WebApi.Extensions;
+global using Comanda.Infrastructure.Constants;
 global using Comanda.Infrastructure.IoC.Extensions;
 global using Comanda.Application.Payloads;
 
